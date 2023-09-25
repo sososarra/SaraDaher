658b2cb7054e7d939513431c74f2dddbb99f436c
sara <sdaher3@uwo.ca>
2023-09-24 20:14:54 -0400

    wesbite change

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-//scroll sections
+const { animate } = require("framer-motion");
 
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,11 +26,28 @@ window.onscroll = () => {
                 links.classList.remove('active')
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
         }
     })
 
     //sticky header
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky',window.scrollY > 100)
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    // remove toogle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animate footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+
+
 }
